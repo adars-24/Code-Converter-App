e4dceeb1adc89ec74d63fcba74af98ba0785f171
Adarsh Singh <adarshsinghciaz@gmail.com>
2025-11-22 23:32:58 +0530

    changed localhost to deployed link

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    const response = await axios.post('https://code-converter-app-olu3.onrender.com/ai/get-review', { code })
     setReview(response.data)
   }
 
